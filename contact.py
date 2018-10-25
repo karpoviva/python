382de5e99a713f5a66097b7fc9e857d05900c3e6
karpov <karpoviva2016@yandex.ru>
2018-10-25 15:44:47 +0700

    создал отдельный класс Contact

---
+++ contact.py
@@ -0,0 +1,8 @@
+class Contact:
+    def __init__(self, fname, sname, lname, address, email, tel):
+        self.fname = fname
+        self.sname = sname
+        self.lname = lname
+        self.address = address
+        self.email = email
+        self.tel = tel
