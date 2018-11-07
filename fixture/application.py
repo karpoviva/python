810b226b408e9eea02cf5198844f193738025ae1
karpov <karpoviva2016@yandex.ru>
2018-11-07 15:20:29 +0700

    Добавлена фикстура в тест "Создание новой группы"

--- fixture/application.py
+++ fixture/application.py
@@ -11,6 +11,10 @@ class Application:
         wd = self.wd
         wd.get("http://localhost/addressbook/")
 
+    # def open_group_page(self):
+        # wd = self.wd
+        # wd.get("http://localhost/addressbook/group.php")
+
     def login(self, username, password):
         wd = self.wd
         self.open_home_page()
@@ -21,6 +25,28 @@ class Application:
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def create_group(self, group):
+        wd = self.wd
+        wd.find_element_by_link_text("groups").click()
+        # init group creation
+        wd.find_element_by_name("new").click()
+        # fill group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+
+    def return_to_group_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("group page").click()
 
     def create_contact(self, contact):
         wd = self.wd
