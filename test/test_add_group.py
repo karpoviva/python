b17ab56ea6b4b57ccf22f66d35811bbce9f09c6d
karpov <karpoviva2016@yandex.ru>
2018-11-07 09:43:05 +0700

    Созданы пакеты для разных зон ответственности

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
-from group import Group
+from model.group import Group
 
 
 class TestAddGroup(unittest.TestCase):
