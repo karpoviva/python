63700932868cf0142ce7dd2bc7941fb279ed8caf
karpov <karpoviva2016@yandex.ru>
2018-12-04 16:23:48 +0700

    Раскомментировал второй тест, тест редактирования поля header группы

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,12 +4,10 @@ from model.group import Group
 def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="New group"))
-    #app.group.return_to_group_page()
     app.session.logout()
 
 
-# def test_modify_group_header(app):
-#     app.session.login(username="admin", password="secret")
-#     app.group.modify_first_group(Group(header="New header"))
-#     app.group.return_to_group_page()
-#     app.session.logout()
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New header"))
+    app.session.logout()
